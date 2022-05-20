d2c988a284fa5a175b676e0af4e367d84354097f
Brett Bertoni <bbertoni1@gmail.com>
2022-05-19 21:57:25 -0400

    Add csv file, modify schema, add queries code

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,1 +1,21 @@
 -- Queries
+-- determine employees to retire (born 1952-1955)
+SELECT COUNT(first_name), last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--create new table of employees to retire\Module Resources\pewlett-hackard-analysis\Data\Module Resources\pewlett-hackard-analysis\Data
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+select * from retirement_info
+select
--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 
--- Creating tables for PH-EmployeeDB
+select * from departments
 CREATE TABLE employees (
      emp_no int NOT NULL,
      birth_date date NOT NULL,
@@ -22,8 +22,8 @@ CREATE TABLE dept_manager (
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -42,7 +42,7 @@ CREATE TABLE dept_emp (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no, dept_no)
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 CREATE TABLE titles (
@@ -50,6 +50,7 @@ CREATE TABLE titles (
   title varchar NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
+
+select * from titles
